ac1f8d72214e917cdb3842cbf9d661a79560dd75
Gyulshen Abaz <weirdoo2001@gmail.com>
2020-12-21 17:40:21 +0200

    Add Dockerfile

--- src/JobBoard.Api/Startup.cs
+++ src/JobBoard.Api/Startup.cs
@@ -43,8 +43,6 @@ namespace JobBoard.Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "JobBoard.Api v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
